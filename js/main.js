2b2b2d9c3b9a38991dfff75dc14cd33c99f9f6dd
Aggromarus <aggromarus@yandex.ru>
2022-12-16 20:55:40 +0300

    merge branches?

--- js/main.js
+++ js/main.js
@@ -1,69 +1,176 @@
-class ProductList {
-    constructor(container = '.product') {
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+
+class List {
+    constructor(url, container, list = list2) {
         this.container = container;
-        this.goods = []; //сюда добавляется весь список товаров из fetchProducts
-        this.allProducts = []; //массив в который рендерятся новые объекты класса ProductItem после ProductList.render()
-        this._getProducts()
+        this.list = list;
+        this.url = url;
+        this.goods = [];
+        this.allProducts = [];
+        this._init();
     }
-    _getProducts() {
-        fetch('json/catalogData.json')
+    _init() {
+        return false;
+    }
+    getJson(url) {
+        return fetch(url ? url : `${API + this.url}`)
             .then(result => result.json())
-            .then(data => {
-                this.goods = [...data]
-                this.render()
-            })
             .catch(error => {
-            console.log(error)
-        })
+                console.log(error)
+            })
+    }
+    handleData(data) {
+        this.goods = [...data];
+        this.render();
+    }
+    calcSum() {
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
     }
     render() {
-        const block = document.querySelector(this.container); //this.container соответствует container из конструктора класса ProductList
+        const block = document.querySelector(this.container);
         for (let product of this.goods) {
-            const productObj = new ProductItem(product); // экземпляр объекта ProductItem 
+            const productObj = new ProductItem(product);
             this.allProducts.push(productObj);
             block.insertAdjacentHTML('beforeend', productObj.render());
         }
     }
 }
 
-
-class ProductItem {  //класс карточки товара
-    constructor(product) {
-        this.title = product.title;
-        this.price = product.price;
-        this.description = product.description;
-        // this.id = product.id;
-        // this.img = product.img;
+class Item {
+    constructor(el, img = 'https://via.placeholder.com/200x150') {
+        this.product_name = el.product_name;
+        this.price = el.price;
+        // this.description = product.description;
+        this.id_product = el.id_product;
+        this.img = img;
     }
-    render() { 
-        return `<div class="card">
-        <a href="product.html"><img src="image/beard_man.png" alt="beard_man" /></a>
-        <h4>${this.title}</h4>
-        <p>${this.description}</p>
-        <p class="card__price">${this.price}$</p>
-        <button class=""buy-btn>Buy</button>
-                </div>`
+    render() {
+        return `<div class="product-item" data-id="${this.id_product}">
+        <img src ="${this.img}" alt="??img??"></img>
+            <div class = desc>
+            <h3>${this.product_name}</h3>
+            <p>${this.price}$</p>
+            <button class="buy-btn"
+            data-id="${this.id_product}"
+            data-name="${this.product_name}"
+            data-price="${this.price}">Buy</button>
+            </div>
+        </div>`
     }
 }
 
-let list = new ProductList();
+class ProductList extends List {
+    constructor(cart, container = '.product', url = '/catalogData.json') {
+        super(url, container);
+        this.cart = cart;
+        this.getJson()
+            .then(data => this.handleData(data));
+    }
+    _init() {
+        document.querySelector(this.container).addEventListener('click', e => {
+            if (e.target.classList.contains('buy-btn')) {
+                this.cart.addProduct(e.target);
+            }
+        })
+    }
+}
 
-// //исходный массив товаров
-// const products = [
-//     { id: 1, title: 'ELLERY X MO CAPSULE', price: 100, description:"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. " },
-//     { id: 2, title: 'Shirt', price: 50, description:"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. " },
-//     { id: 3, title: 'Hat', price: 10, description:"Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi. " },
-// ];
 
-// // рендер карточки товара в списке 
-// const renderProduct = (title, price, description) => {
-    
-// };
+class ProductItem extends Item { }
 
-// // рендер списка товаров, проходит по каждому элементу renderProduct map`ом и записывает в константу списка товаров на странице 
-// const renderPage = list => {
-//     const productsList = list.map(item => renderProduct(item.title, item.price, item.description));
-//     document.querySelector('.product').innerHTML = productsList.join(' ');
-// }
+class Cart extends List {
+    constructor(container = '.cart-block', url = '/getBasket.json') {
+        super(url, container);
+        this.getJson()
+            .then(data => this.handleData(data.contents));
+    }
+    addProduct(element) {
+        this.getJson(`${API}/addToBasket.json`)
+            .then(data => {
+                if (data.result === 1) {
+                    let productId = +element.dataset['id'];
+                    let find = this.allProducts.find(product => product.id_product === productId);
+                    if (find) {
+                        find.quantity++;
+                        this._updateCart(find);
+                    } else {
+                        let product = {
+                            id_product: productId,
+                            price: +element.dataset['price'],
+                            product_name: element.dataset['name'],
+                            quantity: 1
+                        };
+                        this.goods = [product];
+                        this.render();
+                    }
+                } else {
+                    alert('Error');
+                }
+            })
+    }
+    removeItem(element) {
+        this.getJson(`${API}/deleteFromBasket.json`)
+            .then(data => {
+                if (data.result === 1) {
+                    let productId = +element.dataset['id'];
+                    let find = this.allProducts.find(product => product.id_product === productId);
+                    if (find.quantity > 1) {
+                        find.quantity--;
+                        this._updateCart(find);
+                    } else {
+                        this.allProducts.splice(this.allProducts.indexOf(find), 1);
+                        document.querySelector(`.cart-item[data-id="${productId}"]`).remove();
+                    }
+                } else {
+                    alert('Error');
+                }
+            })
+    }
+    _updateCart(product) {
+        let block = document.querySelector(`.cart-item[data-id="${product.id_product}"]`);
+        block.querySelector('product-quantity').textContent = `Quantity: ${product.quantity}`;
+        block.querySelector('product-price').textContent = `$${product.quantity * product.price}`;
+    }
+    _init() {
+        document.querySelector('.btn-cart').addEventListener('click', () => {
+            document.querySelector(this.container).classList.toggle('invisible');
+        })
+        document.querySelector(this.container).addEventListener('click', e => {
+            if (e.target.classList.contains('del-btn')) {
+                this.removeProduct(e.target);
+            }
+        })
+    }
+}
 
-// renderPage(products);
+class CartItem extends Item {
+    constructor(el, img = 'https://via.placeholder.com/50x100') {
+        super(el, img);
+        this.quantity = el.quantity;
+    }
+    render() {
+        return `<div class='cart-item' data-id='${this.id_product}'>
+            <div class product-bio>
+                <img src='$this.img' alt='any_img'>
+                    <div class='product-desc'>
+                        <p class='product-title'>${this.product_name}</p>
+                        <p class='product-quantity'>quantity: ${this.quantity}</p>
+                        <p class='product-single-price'>$${this.price} each</p>
+                    </div>
+            </div>
+            <div class='right-block'>
+                <p class='product-price'>$${this.quantity * this.price}</p>
+                <button class='del-btn' data-id='${this.id_product}'>&times;</button>
+            </div>
+        </div>`
+    }
+}
+const list2 = {
+    ProductsList: ProductItem,
+    Cart: CartItem
+};
+console.log(list2.constructor)
+let cart = new Cart();
+let product = new ProductList(cart);
+// product.getJson('json/catalogData.json').then(data => product.handleData(data));
